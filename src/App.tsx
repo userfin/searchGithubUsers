7c74eabdbee30b81c2909836b6713bc1f23b0080
userfin <klappauser@gmail.com>
2023-09-18 12:15:21 +0300

    Navbar + home/fav pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom'
+import { HomePage } from './pages/HomePage';
+import { FavouritesPage } from './pages/FavouritesPage';
+import { Navigation } from './components/Navigation';
 
 function App() {
   return (
-    <h1 className='font-bold'>sus</h1>
+    <>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={ <HomePage /> } />
+        <Route path="/favourites" element={ <FavouritesPage /> } />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/pages/FavouritesPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export function FavouritesPage() {
+    return (
+        <div>fav</div>
+    )
+}
